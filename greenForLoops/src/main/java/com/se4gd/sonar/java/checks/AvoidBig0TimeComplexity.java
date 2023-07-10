44b71afccd35513de978993ba3354dd2f319a7e8
yamgurung <ram.gurung1990@gmail.com>
2023-07-10 20:09:18 +0300

    issue fixes in detecting nested loops

--- greenForLoops/src/main/java/com/se4gd/sonar/java/checks/AvoidBig0TimeComplexity.java
+++ greenForLoops/src/main/java/com/se4gd/sonar/java/checks/AvoidBig0TimeComplexity.java
@@ -7,9 +7,11 @@ import java.util.Stack;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
-import org.sonar.plugins.java.api.tree.BaseTreeVisitor;
+import org.sonar.plugins.java.api.tree.BlockTree;
+import org.sonar.plugins.java.api.tree.DoWhileStatementTree;
 import org.sonar.plugins.java.api.tree.ForEachStatement;
 import org.sonar.plugins.java.api.tree.ForStatementTree;
+import org.sonar.plugins.java.api.tree.StatementTree;
 import org.sonar.plugins.java.api.tree.Tree;
 import org.sonar.plugins.java.api.tree.Tree.Kind;
 import org.sonar.plugins.java.api.tree.WhileStatementTree;
@@ -30,7 +32,8 @@ public class AvoidBig0TimeComplexity extends IssuableSubscriptionVisitor{
 		return Arrays.asList(
 				Tree.Kind.FOR_STATEMENT,
 				Tree.Kind.FOR_EACH_STATEMENT,
-				Tree.Kind.WHILE_STATEMENT
+				Tree.Kind.WHILE_STATEMENT,
+				Tree.Kind.DO_STATEMENT
 			);
 	}
 	@Override
@@ -38,27 +41,51 @@ public class AvoidBig0TimeComplexity extends IssuableSubscriptionVisitor{
 		// TODO Auto-generated method stub
 		//super.visitNode(tree);
 		if (tree.is(Kind.FOR_STATEMENT)) {
-            ForStatementTree forLoop = (ForStatementTree) tree;
-            checkNestedLoop(forLoop);
-        } else if (tree.is(Kind.WHILE_STATEMENT)) {
-            WhileStatementTree whileLoop = (WhileStatementTree) tree;
-            checkNestedLoop(whileLoop);
+            ForStatementTree forLoopStatementTree = (ForStatementTree) tree;
+            StatementTree statementTree = forLoopStatementTree.statement();
+            if(checkNestedLoop(statementTree)) {
+            	reportIssue(tree, RULE_MESSAGE);
+            }else {
+            	super.visitNode(statementTree);
+            }
+        }else if (tree.is(Kind.WHILE_STATEMENT)) {
+            WhileStatementTree whileLoopStatementTree = (WhileStatementTree) tree;
+            StatementTree statementTree = whileLoopStatementTree.statement();
+            if(checkNestedLoop(statementTree)) {
+            	reportIssue(tree, RULE_MESSAGE);
+            }else {
+            	super.visitNode(statementTree);
+            }
         }else if(tree.is(Kind.FOR_EACH_STATEMENT)) {
         	ForEachStatement forEachStatement = (ForEachStatement) tree;
-        	checkNestedLoop(forEachStatement);
+        	StatementTree statementTree = forEachStatement.statement();
+        	
+        	if(checkNestedLoop(statementTree)) {
+            	reportIssue(tree, RULE_MESSAGE);
+            }else {
+            	super.visitNode(statementTree);
+            }
+        }
+        else if(tree.is(Kind.DO_STATEMENT)) {
+        	DoWhileStatementTree doWhileStatementTree = (DoWhileStatementTree) tree;
+        	StatementTree statementTree = doWhileStatementTree.statement();
+            if(checkNestedLoop(statementTree)) {
+            	reportIssue(tree, RULE_MESSAGE);
+            }else {
+            	super.visitNode(statementTree);
+            }
         }
 	}
-	
-	private void checkNestedLoop(Tree loop) {
-        if (!loopStack.isEmpty() && isNestedLoop(loop)) {
-            loopStack = new Stack<>();
-            reportIssue(loop, RULE_MESSAGE);
-        }else {
-        	loopStack.push(loop.kind());
-        }
-    }
-
-    private boolean isNestedLoop(Tree loop) {
-        return loopStack.contains(loop.kind());
-    }
+	private boolean checkNestedLoop(StatementTree statementTree) {
+	    if (statementTree instanceof BlockTree) {
+	      BlockTree block = (BlockTree) statementTree;
+	      for (StatementTree nestedStatement : block.body()) {
+	        if (nestedStatement.is(Tree.Kind.FOR_STATEMENT) || nestedStatement.is(Tree.Kind.WHILE_STATEMENT)
+	                || nestedStatement.is(Tree.Kind.DO_STATEMENT) || nestedStatement.is(Tree.Kind.FOR_EACH_STATEMENT)) {
+	          return true;
+	        }
+	      }
+	    }
+	    return false;
+	  }
 }
--- greenForLoops/src/test/files/AvoidBig0TimeComplexityNonCompliant.java
+++ greenForLoops/src/test/files/AvoidBig0TimeComplexityNonCompliant.java
@@ -7,6 +7,15 @@ public class AvoidBig0TimeComplexityNonCompliant {
 				System.out.println("Hello world");
 			}
 		}
+		for(int j = 0; j < 5; j++) {
+			System.out.println("Hello world 1");
+		}
+		for(int j = 0; j < 5; j++) {
+			System.out.println("Hello world 1");
+		}
+		for(int j = 0; j < 5; j++) {
+			System.out.println("Hello world 1");
+		}
 		for (int i = 0; i < 5; i++) {
 			for(int j = 0; j < 5; j++) {
 				System.out.println("Hello world");
@@ -32,6 +41,38 @@ public class AvoidBig0TimeComplexityNonCompliant {
 		while(k < 5) {
 			System.out.println("Hello world");
 			k++;
+		}
+		int k = 0;
+		while(k < 5) {
+			System.out.println("Hello world");
+			k++;
+		}
+		int k = 0;
+		while(k < 5) {
+			System.out.println("Hello world");
+			k++;
+		}
+		int l = 0;
+		int m = 0;
+		while(l < 5) {
+			System.out.println("Hello world");
+			while (m < 5) {
+				System.out.println("Hello world 1");
+				m++;
+			}
+			l++;
+		}
+		
+		int[] a = {1,2,3,4,5};
+		int[] b = {1,2,3,4,5};
+		for (int n : b) {
+			for (int o : a) {
+				
+			}
+		}
+		
+		for (int n : b) {
+			
 		}
 	}
 }
